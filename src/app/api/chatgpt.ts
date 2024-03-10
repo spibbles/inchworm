36e23a22a8fa4fa2b91e502d0713b2469445ab44
tumi! <fabiyitumi@gmail.com>
2024-03-09 21:05:33 -0800

    got routing to work

--- src/app/api/chatgpt.ts
+++ src/app/api/chatgpt.ts
@@ -1,33 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
 
-export default async function chatgpt(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { messages } = req.body
-  const apiKey = process.env.OPENAI_API_KEY
-  const url = 'https://api.openai.com/v1/chat/completions'
+// export default async function chatgpt(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { messages } = req.body
+//   const apiKey = process.env.OPENAI_API_KEY
+//   const url = 'https://api.openai.com/v1/chat/completions'
   
-  const body = JSON.stringify({
-    messages,
-    model: 'gpt-3.5-turbo',
-    stream: false,
-  })
+//   const body = JSON.stringify({
+//     messages,
+//     model: 'gpt-3.5-turbo',
+//     stream: false,
+//   })
 
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body,
-    })
-    const data = await response.json()
-    res.status(200).json({ data })
-    console.log(data)
-  } catch (error) {
-    res.status(500).json({ error: error })
-  }
-}
+//   try {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//       body,
+//     })
+//     const data = await response.json()
+//     res.status(200).json({ data })
+//     console.log(data)
+//   } catch (error) {
+//     res.status(500).json({ error: error })
+//   }
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,30 @@ import Image from "next/image";
 import {useEffect} from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('/api/chatgpt', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({messages:}),
-      })
-      return await response.json()
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch('/api/chatgpt', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({messages:"string"}),
+  //     })
+  //     return await response.json()
+  //   })()
+  // }, [])
 
 
   return (
     <main>
-      <input
+      {/* <input
         placeholder="Email"
         type="email"></input>
       <input
         placeholder="Password"
         type="password"></input>
-      {/*<a href="/sign-in">Sign in</a>*/}
+      <a href="/sign-in">Sign in</a> */}
+      <p> Hello!</p>
 
       <button onClick = {async () =>{
          const response = await fetch("/api/chatgpt", {
@@ -33,7 +34,7 @@ export default function Home() {
           headers:{
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({
+          body: JSON.stringify({  
             someData: true,
           })
          });
