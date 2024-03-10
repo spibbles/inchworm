1a6d0111448eb8bc1ecbc65733c341c350168971
Christina <christinaraganit@gmail.com>
2024-03-09 22:58:51 -0800

    add email to db

--- inchworm/src/app/page.tsx
+++ inchworm/src/app/page.tsx
@@ -3,43 +3,48 @@
 "use client";
 
 import Image from "next/image";
-import {useEffect} from 'react';
-import { auth, db} from "../lib/firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
-import {signInWithGoogle, signOut} from "../lib/firebase/auth";
-
+import { useEffect } from "react";
+import { auth, db } from "../lib/firebase/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { signInWithGoogle, signOut } from "../lib/firebase/auth";
+import { collection } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 export default function Home() {
   useEffect(() => {
     (async () => {
-      const response = await fetch('/api/chatgpt', {
-        method: 'POST',
+      const response = await fetch("/api/chatgpt", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ messages: 'i like icecream' }),
-      })
-      return await response.json()
-    })( )
-  }, [])
+        body: JSON.stringify({ messages: "i like icecream" }),
+      });
+      return await response.json();
+    })();
+  }, []);
   const handleSubmit = () => {
-    console.log("runs here")
+    console.log("runs here");
     signInWithGoogle();
-  }
+  };
+
+  const addToDB = () => {
+    db.collection("users").add({
+      email: "hello@hello.com",
+      name: "Chris",
+    });
+  };
 
   return (
     <main>
-      <form action="" onSubmit={handleSubmit}
-      >
-        <input
-        placeholder="Email"
-        type="email"></input>
-      <input
-        placeholder="Password"
-        type="password"></input>
-      <button type="submit">Sign in</button>
-      </form>
-      
+      <button onClick={handleSubmit}>Sign in</button>
+   
+      <button
+        onClick={() => {
+          setDoc(doc(db, "users", "Christina"), { email: "hello@chris.app" });
+        }}>
+        Add stuff to db
+      </button>
     </main>
   );
 }
--- inchworm/src/lib/firebase/auth.js
+++ inchworm/src/lib/firebase/auth.js
@@ -1,34 +1,47 @@
 import {
-    GoogleAuthProvider,
-    signInWithPopup,
-    onAuthStateChanged as _onAuthStateChanged,
-  } from "firebase/auth";
-  
-  import { auth } from "./firebase";
-  
-  export function onAuthStateChanged(cb) {
-      return _onAuthStateChanged(auth, cb);
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged as _onAuthStateChanged,
+} from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { db } from "./firebase";
+import { setDoc, doc } from "firebase/firestore";
+import { auth } from "./firebase";
+import { collection } from "firebase/firestore";
+
+export function onAuthStateChanged(cb) {
+  return _onAuthStateChanged(auth, cb);
+}
+
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+
+  try {
+    console.log("It gets here");
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user.uid);
+        console.log(user.email);
+        setDoc(doc(db, "users", user.uid), { email: user.email });
+      })
+      .catch(function (error) {
+        console.log("error", error.message);
+      });
+    console.log("It gets all the way here");
+  } catch (error) {
+    console.error("Error signing in with Google", error);
   }
-  
-  export async function signInWithGoogle() {
-    console.log("This part works :3")
-    const provider = new GoogleAuthProvider();
-    
-    try {
-        console.log("does it go here")
-      const result = await signInWithPopup(auth, provider);
-      await signInWithPopup(auth, provider);
-      console.log(result.user);
-    } catch (error) {
-      console.error("Error signing in with Google", error);
-    }
+}
+
+export async function signOut() {
+  try {
+    return auth.signOut();
+  } catch (error) {
+    console.error("Error signing out with Google", error);
   }
-  
-  export async function signOut() {
-    try {
-      return auth.signOut();
-    } catch (error) {
-      console.error("Error signing out with Google", error);
-    }
-  }
-  
+}
